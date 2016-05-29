8f08b6bd625f8efb75cce0ea521a0657041d441c
mstg <mustiigezen@gmail.com>
2016-05-29 06:56:43 +0200

    Initial file commit

---
+++ lib/gcc/i686-elf/7.0.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc/configure --target=i686-elf --prefix=/Users/mustafa/dev/os/cross --disable-nls --enable-languages=c,c++ --without-headers --with-gmp=/usr/local/Cellar/gmp/6.0.0a --with-mpfr=/usr/local/Cellar/mpfr/3.1.3 --with-mpc=/usr/local/Cellar/libmpc/1.0.3";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "pentiumpro" } };
